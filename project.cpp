ad13afac17208e0a1b301a8ca47f1ad3c666a59e
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2017-11-19 23:03:33 +0800

    Proses Case 5

--- project.cpp
+++ project.cpp
@@ -107,6 +107,34 @@ int main(){
 					cout<<"Harap Pilih Jenis Bidang yang Benar!!!";
 				}
 				break;
+			case 5:
+				system("cls");
+				int LuasRumah;
+	
+				double LuasParket = 193*1215;
+				
+				double Box = LuasParket*8/1000000;
+				
+				double JumlahBox, Parket, Pasang, Total;
+				cout<<"Luas Rumah        :  ";
+				cin>>LuasRumah;
+				cout<<"-------------------------------- \n";
+				
+				cout<<"Luas Rumah        : "<<LuasRumah<<" m2"<<endl;
+				cout<<"Luas 1 Box Parket : "<<Box<<" m2"<<endl;
+				
+				JumlahBox = LuasRumah/Box;
+				cout<<"Jumlah Box Parket : "<<JumlahBox<<endl;
+				
+				Parket = JumlahBox * 500;
+				cout<<"Biaya Parket(Rp)  : "<<Parket<<endl;
+				
+				Pasang = LuasRumah * 20;
+				cout<<"Biaya Pasang(Rp)  : " <<Pasang<<endl;
+				
+				Total = Parket + Pasang;
+				cout<<"Total Biaya(Rp)   : " <<Total<<endl;
+				break;
 		}
 	}
 	
