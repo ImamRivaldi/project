afe746a333254580ae01d6b43c3d13a75474e3d4
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2017-11-19 22:52:37 +0800

    Proses Case 2

--- project.cpp
+++ project.cpp
@@ -40,6 +40,31 @@ int main(){
 					cout<<"Hasil	: Gemuk"<<endl;
 				}
 				break;
+			case 2:
+				system("cls");
+				int x,y,z;
+				cout<<"Masukkan Nilai X = ";
+				cin>>x;
+				
+				cout<<"Masukkan Nilai Y = ";
+				cin>>y;
+				
+				if(x>y){
+					z=y-x;
+					cout<<abs(z)<<endl;
+					cout<<"Hasil Positif";
+				}
+				else if(y>x){
+					z=x-y;
+					cout<<abs(z)<<endl;
+					cout<<"Hasil Positif";
+				}
+				else if(z==0){
+					z=z*(-1);
+					cout<<abs(x-y)<<endl;
+					cout<<"Hasil = 0 (Nilai Mutlak)";
+				}
+				break;
 		}
 	}
 	
