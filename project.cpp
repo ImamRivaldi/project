3156d95989f37573476bb11f05715368324c1630
ImamRivaldi <32257746+ImamRivaldi@users.noreply.github.com>
2017-11-19 22:40:44 +0800

    Membuat Pilihan Program Untuk Dijalankan

--- project.cpp
+++ project.cpp
@@ -6,5 +6,26 @@
 using namespace std;
 
 int main(){
+	int pilihan;
+	char pilih;
+	ulang:
+	do {
+		system("cls");
+		cout<<"Silahkan Pilih Proses Program : \n";
+		cout<<"+++++++++++++++++++++++++++++++ \n";
+		cout<<"1. Berat Ideal \n";
+		cout<<"2. Nilai Absolute \n";
+		cout<<"3. Menentukan Nilai \n";
+		cout<<"4. Menghitung Luas \n";
+		cout<<"5. Menghitung Pemasangan Parket \n";
+		cout<<"=============================== \n";
+		cout<<"Masukkan Pilihan : ";
+		cin>>pilihan;
+	}
 	
+	while(pilih=='Y'||pilih=='y');
+	system("cls");
+	cout<<"Terima Kasih Telah Mencoba Program Saya, Silahkan Kembali Lain Waktu :)";
+	getch();
+	return 0;
 }
